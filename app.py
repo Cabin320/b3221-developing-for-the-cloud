00ef1961544aba558197a482b771bffc0d626989
Cabin320 <jacob.hogarty@yahoo.com>
2024-03-06 20:59:12 +0000

    base set up

---
+++ app.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Home"})
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", reload=True)
