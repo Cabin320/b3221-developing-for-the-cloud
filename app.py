d4fc4753b081505d14d9e8b3f5f6bb381f827657
Cabin320 <jacob.hogarty@yahoo.com>
2024-03-16 17:31:52 +0000

    Adding pydantic data validation classes

--- app.py
+++ app.py
@@ -23,11 +23,19 @@ class DogWalkerInfo(BaseModel):
     password: str
 
 
+class AdditionalPetInfo(BaseModel):
+    name: str
+    breed: str
+    age: int
+
+
 class DogOwnerInfo(BaseModel):
     email: str
     password: str
     dog: str
-    add_dog: Optional[List[str]] = None
+    breed: str
+    age: int
+    add_pet: Optional[List[AdditionalPetInfo]] = None
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -43,16 +51,44 @@ async def register_page(request: Request):
 @app.post("/submit", response_class=HTMLResponse)
 async def submit_registry_info(request: Request):
     form_data = await request.form()
-    email = form_data.get("inputEmail")
-    password = form_data.get("inputPassword")
-    dog = form_data.get("dog")
-    add_dog = form_data.getlist("add_dog")
 
-    dog_walker_info = DogWalkerInfo(email=email, password=password)
+    email = form_data.get("email")
+    password = form_data.get("password")
 
-    dog_owner_info = DogOwnerInfo(email=email, password=password, dog=dog, add_dog=add_dog)
+    if form_data.get("dog_walker") is not None:
+        if email and password:
+            dog_walker_info = DogWalkerInfo(email=email, password=password)
 
-    return templates.TemplateResponse("registration.html", {"request": request, "submitted": True})
+            print(dog_walker_info)
+            return templates.TemplateResponse("registration.html", {"request": request, "submitted": True})
+
+    if form_data.get("dog_owner") is not None:
+        dog = form_data.get("dog")
+        breed = form_data.get("breed")
+        age = form_data.get("age")
+
+        add_dog = form_data.getlist("add_dog")
+        add_breed = form_data.getlist("add_breed")
+        add_age = form_data.getlist("add_age")
+
+        additional_pets = []
+        for pet_name, pet_breed, pet_age in zip(add_dog, add_breed, add_age):
+            AdditionalPetInfo(name=pet_name, breed=pet_breed, age=int(pet_age))
+
+        if email and password and dog and breed and age:
+            dog_owner_info = DogOwnerInfo(
+                email=email,
+                password=password,
+                dog=dog,
+                breed=breed,
+                age=int(age),
+                add_pet=additional_pets
+            )
+
+            print(dog_owner_info)
+            return templates.TemplateResponse("registration.html", {"request": request, "submitted": True})
+
+    return templates.TemplateResponse("registration.html", {"request": request, "submitted": False})
 
 
 if __name__ == "__main__":
