5958ec9b8ced8665039d62194a369226d5f6e30a
Cabin320 <jacob.hogarty@yahoo.com>
2024-03-16 15:28:45 +0000

    updating ui and adding login and registration option

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import uvicorn
 from typing import List, Optional
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -18,8 +18,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-class RegistryInfo(BaseModel):
-    dog_walker: str
+class DogWalkerInfo(BaseModel):
+    email: str
+    password: str
+
+
+class DogOwnerInfo(BaseModel):
+    email: str
+    password: str
     dog: str
     add_dog: Optional[List[str]] = None
 
@@ -29,20 +35,24 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+@app.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return templates.TemplateResponse("registration.html", {"request": request})
+
+
 @app.post("/submit", response_class=HTMLResponse)
 async def submit_registry_info(request: Request):
     form_data = await request.form()
-    dog_walker = form_data.get("dog_walker")
+    email = form_data.get("inputEmail")
+    password = form_data.get("inputPassword")
     dog = form_data.get("dog")
     add_dog = form_data.getlist("add_dog")
 
-    if not dog_walker or not dog:
-        return templates.TemplateResponse("index.html", {"request": request, "missing": True})
+    dog_walker_info = DogWalkerInfo(email=email, password=password)
 
-    registry_info = RegistryInfo(dog_walker=dog_walker, dog=dog, add_dog=add_dog)
+    dog_owner_info = DogOwnerInfo(email=email, password=password, dog=dog, add_dog=add_dog)
 
-    print(registry_info.dog_walker, registry_info.dog, registry_info.add_dog)
-    return templates.TemplateResponse("index.html", {"request": request, "submitted": True})
+    return templates.TemplateResponse("registration.html", {"request": request, "submitted": True})
 
 
 if __name__ == "__main__":
